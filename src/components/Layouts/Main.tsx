534a63c47e84b5eace357ee9499d358dc9705dff
zachuri <punsalang.zachary@gmail.com>
2022-09-24 22:28:07 -0700

    Layouts: added layout for my main (reusuable layout for all pages)

--- src/components/Layouts/Main.tsx
+++ src/components/Layouts/Main.tsx
@@ -8,8 +8,10 @@ interface mainProps {
 const Main: React.FC<mainProps> = ({ children }) => {
   return (
     <main>
-      <div className='container max-w-md pt-20'>
-        {children}
+      <div className='flex flex-col justify-center items-center pt-20 px-10'>
+        <div className="max-w-md flex-1 flex flex-col justify-center items-center pt-10 lg:pt-6">
+          {children}
+        </div>
       </div>
     </main>
 
