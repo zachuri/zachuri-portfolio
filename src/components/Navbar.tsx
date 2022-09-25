6c6f6423bc87fc729bad19dbb6c490f380f014e3
zachuri <punsalang.zachary@gmail.com>
2022-09-25 01:56:31 -0700

    navbar: fixed hamburger menu icons height to align with toggle switch

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -46,7 +46,7 @@ const Navbar: React.FC = () => {
         <div>
           <ul className='hidden md:flex'>
             <Link href="/">
-              <li className="ml-10 text-sm uppercase hover:border-b">
+              <li className="ml-10 text-sm uppercase border-b">
                 WHO
               </li>
             </Link>
@@ -65,15 +65,13 @@ const Navbar: React.FC = () => {
 
         {/* Right (medium and large)*/}
         <div className='justify-between items-center hidden md:flex'>
-          <button onClick={handleTheme} className="flex justify-between items-center ">
+          <button onClick={handleTheme} className="flex justify-between items-center mr-2">
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
               <path strokeLinecap="round" strokeLinejoin="round" d="M21.752 15.002A9.718 9.718 0 0118 15.75c-5.385 0-9.75-4.365-9.75-9.75 0-1.33.266-2.597.748-3.752A9.753 9.753 0 003 11.25C3 16.635 7.365 21 12.75 21a9.753 9.753 0 009.002-5.998z" />
             </svg>
           </button>
           <Link href="/contact">
-            <div className='ml-2'>
-              CONTACT
-            </div>
+            CONTACT
           </Link>
         </div>
 
@@ -84,13 +82,11 @@ const Navbar: React.FC = () => {
               <path strokeLinecap="round" strokeLinejoin="round" d="M21.752 15.002A9.718 9.718 0 0118 15.75c-5.385 0-9.75-4.365-9.75-9.75 0-1.33.266-2.597.748-3.752A9.753 9.753 0 003 11.25C3 16.635 7.365 21 12.75 21a9.753 9.753 0 009.002-5.998z" />
             </svg>
           </button>
-          <Menu as="div" className="relative inline-block text-left">
+          <Menu as="div" className="relative inline-block text-left mt-1">
             <Menu.Button>
-              <div className="flex lg:hidden">
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
                   <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
                 </svg>
-              </div>
             </Menu.Button>
 
             <Transition
