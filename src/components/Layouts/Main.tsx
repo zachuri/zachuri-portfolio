902bcf3f6e848340fde7db92a97e04a4513b70ae
zachuri <punsalang.zachary@gmail.com>
2022-09-25 02:57:42 -0700

    navbar/layout: fixed padding issue for lg screen

--- src/components/Layouts/Main.tsx
+++ src/components/Layouts/Main.tsx
@@ -8,8 +8,8 @@ interface mainProps {
 const Main: React.FC<mainProps> = ({ children }) => {
   return (
     <main>
-      <div className='flex flex-col justify-center items-center pt-10 md:pt-20 px-10'>
-        <div className="max-w-md flex-1 flex flex-col justify-center items-center pt-r md:pt-10 lg:pt-6">
+      <div className='flex flex-col justify-center items-center px-10 pt-10 md:pt-20 '>
+        <div className="max-w-md flex-1 flex flex-col justify-center items-center pt-6">
           {children}
         </div>
       </div>
