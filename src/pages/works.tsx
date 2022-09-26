f3d90a67343b469af0b19033ac580d7a3213f885
zachuri <punsalang.zachary@gmail.com>
2022-09-25 21:15:56 -0700

    works page: start

---
+++ src/pages/works.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import MainLayout from '../components/Layouts/Main';
+
+const Works = () => {
+  return (
+    <>
+      <MainLayout>
+        <h1>Works</h1>
+      </MainLayout>
+    </>
+  )
+}
+
+export default Works;
