ac9c04b1aad288b3fab0aaec31ff4025147ab7fa
zachuri <punsalang.zachary@gmail.com>
2022-09-26 00:52:24 -0700

    article layout: added component to add framer-motion to components

--- src/components/Layouts/Article.tsx
+++ src/components/Layouts/Article.tsx
@@ -1,9 +1,25 @@
 import React from 'react'
+import { motion } from "framer-motion"
 
-const Article = () => {
+const variants = {
+  hidden: { opacity: 0, x: 0, y: 20 },
+  enter: { opacity: 1, x: 0, y: 0 },
+  exit: { opacity: 0, x: -0, y: 20 }
+}
+
+const Layout: React.FC<{ children: React.ReactNode }> = ({ children }) => {
   return (
-    <div>Article</div>
+    <motion.article
+      initial="hidden"
+      animate="enter"
+      exit="exit"
+      variants={variants}
+      transition={{ duration: 0.4, type: 'easeInOut' }}
+      style={{ position: 'relative' }}
+    >
+      {children}
+    </motion.article>
   )
 }
 
-export default Article
+export default Layout
