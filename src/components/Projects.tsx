dd40b515f98f5a8fbfadc76d2915a1ae7cc8b83a
Zachary <punsalang.zachary@gmail.com>
2022-09-27 22:14:39 +0000

    added ecommerce-movie to works

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -5,11 +5,7 @@ const Projects: React.FC = () => {
   return (
     <div className="mt-5 grid grid-cols-1 md:grid-cols-2 gap-6 ">
       <Project id={"twitch-clone"} name={"twitch-clone"} />
-      <Project id={"twitch-clone"} name={"twitch-clone"} />
-      <Project id={"twitch-clone"} name={"twitch-clone"} />
-      <Project id={"twitch-clone"} name={"twitch-clone"} />
-      <Project id={"twitch-clone"} name={"twitch-clone"} />
-      <Project id={"twitch-clone"} name={"twitch-clone"} />
+      <Project id={"ecommerce-movie"} name={"Ecommerce-movie"} />
     </div>
   )
 }
