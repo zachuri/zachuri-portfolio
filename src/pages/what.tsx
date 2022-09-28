1b1d8ee39dcd011856402dd52c8e1e292ac4ba47
zachuri <punsalang.zachary@gmail.com>
2022-09-28 15:13:11 -0700

    what: added skills

--- src/pages/what.tsx
+++ src/pages/what.tsx
@@ -1,12 +1,23 @@
 import React from 'react'
 import MainLayout from '../components/Layouts/Main';
 import LayoutMotion from '../components/Layouts/Motion';
+import Skills from '../components/Skills';
+import Skill from '../components/Skill';
 
 const What: React.FC = () => {
   return (
     <MainLayout>
       <LayoutMotion>
-        <div>HELLO WHAT</div>
+        <div>
+          <h1>Technical</h1>
+          <h2>skills</h2>
+          <div className='grid grid-cols-4 '>
+            <Skills />
+          </div>
+        </div>
+        <div>
+          <h1>Personal</h1>
+        </div>
       </LayoutMotion>
     </MainLayout>
   )
