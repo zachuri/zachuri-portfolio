f0bbe0b4b51ccf9eef930c6a2d76817fe79e394f
zachuri <punsalang.zachary@gmail.com>
2022-09-28 01:56:09 -0700

    footer: setup finish

---
+++ src/components/Footer.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Footer = () => {
+
+  return (
+    <div className="text-center opacity-25 text-sm mt-10">
+      &copy; {new Date().getFullYear()} Zachary Punsalang. All Rights Reserved.
+    </div>
+  )
+}
+
+export default Footer
