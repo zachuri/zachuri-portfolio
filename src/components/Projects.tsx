63fcdb3ede8d84f5e3fcccade30fbd62d13f042b
ZACHURI <punsalang.zachary@gmail.com>
2022-09-28 03:22:19 -0700

    projects: fixed typo for diversity-hotline

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -6,7 +6,7 @@ const Projects: React.FC = () => {
     <div className="mt-5 grid grid-cols-1 md:grid-cols-2 gap-6 ">
       <Project id={"twitch-clone"} title={"Twitch Clone"} desc={"Clone of Twitch using next.js and tailwind.css to help my understanding of design"} />
       <Project id={"ecommerce-movie"} title={"Ecommerce Movie"} desc={"Full Stack ecommerce movie shop where users are able to buy movies"} />
-      <Project id={"diversity-hotline"} title={"Diversity Hotline"} desc={"Hotline for individuals to discuss topics regarind diversity, equity, inclusion, and discrimination"} />
+      <Project id={"diversity-hotline"} title={"Diversity Hotline"} desc={"Hotline for individuals to discuss topics regarding diversity, equity, inclusion, and discrimination"} />
     </div>
   )
 }
