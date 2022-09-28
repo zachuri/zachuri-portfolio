68a1a99059ee3b37c14456c94c4fda258086c4e8
zachuri <punsalang.zachary@gmail.com>
2022-09-27 23:09:49 -0700

    twitch-clone page: added twitch-clone/ rename from twitchclone -> twitch-clone

---
+++ src/pages/works/twitch-clone.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Layout from '../../components/Layouts/Main'
+
+const twitchclone = () => {
+  return (
+    <div>
+      <Layout>
+        <h1>twitch clone</h1>
+      </Layout>
+    </div>
+  )
+}
+
+export default twitchclone
--- src/pages/works/twitchclone.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const twitchclone = () => {
-  return (
-    <div>twitchclone</div>
-  )
-}
-
-export default twitchclone
