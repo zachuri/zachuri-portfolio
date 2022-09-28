9f37440157c7fd6fe8d852861e3300e44bae029d
zachuri <punsalang.zachary@gmail.com>
2022-09-28 02:34:36 -0700

    diversity-hotline: added page/ filled with content

--- src/pages/works/diversity-hotline.tsx
+++ src/pages/works/diversity-hotline.tsx
@@ -1,13 +1,85 @@
+import Image from 'next/image';
 import React from 'react'
 import MainLayout from '../../components/Layouts/Main';
 import LayoutMotion from '../../components/Layouts/Motion';
+import { Badge } from '../../components/Project';
 
 const DiversityHotline = () => {
   return (
     <div>
       <MainLayout>
         <LayoutMotion>
-          <h1>DiversityHotline</h1>
+          <h1 className='text-2xl font-bold dark:text-[#bd93f9] text-center mb-5'> <span className='text-base font-normal text-black dark:text-white'>Works &gt;</span> Diversity Hotline</h1>
+          <p className='indent-8 mb-5'>
+            Collaborated with Student Service Counselor of UCI who wanted a Progressive Web Application where
+            mentors and users can schedule counseling meetings and users can access multiple resources regarding
+            diversity, equity and inclusion.
+          </p>
+
+          <ul className='mb-5'>
+            <li>
+              <Badge>
+                Website
+              </Badge>
+              <a href="https://suspicious-euclid-dddcf0.netlify.app/">DiversityHotline.com</a>
+            </li>
+            <li>
+              <Badge>
+                Platform
+              </Badge>
+              Desktop, Mobile (Progressive Web Application)
+            </li>
+            <li>
+              <Badge>
+                Stack
+              </Badge>
+              React.js, CSS
+            </li>
+            <li>
+              <Badge>
+                Source
+              </Badge>
+              <a href="https://github.com/zachuri/DiversityHotlineApp">https://github.com/zachuri/DiversityHotlineApp</a>
+            </li>
+          </ul>
+
+          <div className='flex flex-col gap-8'>
+            <Image
+              src={`/assets/projects/diversity-hotline.png`}
+              alt={"twitch-clone"}
+              width="1200"
+              height="700"
+              className='rounded-xl mb-5'
+              loading="lazy"
+            />
+
+            <Image
+              src={`/assets/projects/diversity-hotline-1.png`}
+              alt={"twitch-clone"}
+              width="1200"
+              height="700"
+              className='rounded-xl mb-5'
+              loading="lazy"
+            />
+
+            <Image
+              src={`/assets/projects/diversity-hotline-2.png`}
+              alt={"twitch-clone"}
+              width="1200"
+              height="700"
+              className='rounded-xl mb-5'
+              loading="lazy"
+            />
+
+            <Image
+              src={`/assets/projects/diversity-hotline-3.png`}
+              alt={"twitch-clone"}
+              width="1200"
+              height="700"
+              className='rounded-xl mb-5'
+              loading="lazy"
+            />
+          </div>
         </LayoutMotion>
       </MainLayout>
     </div>
