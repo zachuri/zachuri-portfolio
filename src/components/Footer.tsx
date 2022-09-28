5353d16e1a0352361b2f2cfff813a6272cf507eb
zachuri <punsalang.zachary@gmail.com>
2022-09-28 01:58:43 -0700

    footer: added layout motion

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,11 +1,14 @@
 import React from 'react'
+import LayoutMotion from './Layouts/Motion';
 
 const Footer = () => {
 
   return (
-    <div className="text-center opacity-25 text-sm mt-10">
-      &copy; {new Date().getFullYear()} Zachary Punsalang. All Rights Reserved.
-    </div>
+    <LayoutMotion>
+      <div className="text-center opacity-25 text-sm p-4">
+        &copy; {new Date().getFullYear()} Zachary Punsalang. All Rights Reserved.
+      </div>
+    </LayoutMotion>
   )
 }
 
