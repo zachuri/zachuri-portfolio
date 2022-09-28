bb5b6f4feb57a7f46a061856e63ace8019c29cf4
zachuri <punsalang.zachary@gmail.com>
2022-09-28 01:14:01 -0700

    project: added badge component

--- src/components/Project.tsx
+++ src/components/Project.tsx
@@ -9,6 +9,13 @@ interface Props {
   desc: string;
 }
 
+
+export const Badge: React.FC<{ children: string }> = ({ children }) => {
+  return (
+    <span className="dark:bg-[#bd93f9] dark:text-black text-xs font-semibold mr-2 px-2.5 py-0.5 rounded bg-purple-200 text-purple-900">{children}</span>
+  )
+}
+
 const Project: React.FC<Props> = ({ id, title, desc }) => {
   return (
     <div>
