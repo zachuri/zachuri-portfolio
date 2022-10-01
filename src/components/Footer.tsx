e43e54704dc2b8f8178aae8f1ddcca8fabee4c6a
zachuri <punsalang.zachary@gmail.com>
2022-09-30 19:57:09 -0700

    footer: added my contacts in the footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import LayoutMotion from './Layouts/Motion';
+import { AiOutlineMail } from "react-icons/ai";
+import { FaLinkedin, FaGithub, FaTwitter } from "react-icons/fa";
 
 const Footer = () => {
 
@@ -7,6 +9,20 @@ const Footer = () => {
     <LayoutMotion>
       <div className="text-center opacity-25 text-sm p-4">
         &copy; {new Date().getFullYear()} Zachary Punsalang. All Rights Reserved.
+        <div className='flex flex-cols gap-5 justify-center'>
+          <a href="https://www.linkedin.com/in/zachary-punsalang/">
+            <FaLinkedin className='fill-black dark:fill-white' />
+          </a>
+          <a href="https://github.com/zachuri">
+            <FaGithub className='fill-black dark:fill-white' />
+          </a>
+          <a href="https://twitter.com/zachurii">
+            <FaTwitter className='fill-black dark:fill-white' />
+          </a>
+          <a href="mailto:punsalang.zachary.work@gmial.com?body=">
+            <AiOutlineMail className='fill-black dark:fill-white' />
+          </a>
+        </div>
       </div>
     </LayoutMotion>
   )
