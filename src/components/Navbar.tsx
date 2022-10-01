1a8ac31eabfd554255801c67caf4e2d8f4d8023e
zachuri <punsalang.zachary@gmail.com>
2022-09-30 20:22:46 -0700

    navbar: accessibilitychecker-> place label on div instead of button

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -76,11 +76,12 @@ const Navbar: React.FC = () => {
         </div>
 
         {/* Right (medium and large)*/}
-        <div
-          className='justify-between items-center hidden md:flex'
-          role="toggle" aria-label="toggle between light and dark mode"
-        >
-          <button onClick={handleTheme} className="flex justify-between items-center mr-2">
+        <div className='justify-between items-center hidden md:flex'>
+          <button
+            onClick={handleTheme}
+            className="flex justify-between items-center mr-2"
+            role="toggle" aria-label="toggle between light and dark mode"
+          >
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
               <path strokeLinecap="round" strokeLinejoin="round" d="M21.752 15.002A9.718 9.718 0 0118 15.75c-5.385 0-9.75-4.365-9.75-9.75 0-1.33.266-2.597.748-3.752A9.753 9.753 0 003 11.25C3 16.635 7.365 21 12.75 21a9.753 9.753 0 009.002-5.998z" />
             </svg>
@@ -91,11 +92,12 @@ const Navbar: React.FC = () => {
         </div>
 
         {/* Right (Hamburger)*/}
-        <div
-          className='flex justify-between items-center md:hidden'
-          role="hamgurger menu" aria-label="view the links to all the other pages"
-        >
-          <button onClick={handleTheme} className="flex justify-between items-center mr-2">
+        <div className='flex justify-between items-center md:hidden'>
+          <button
+            onClick={handleTheme}
+            className="flex justify-between items-center mr-2"
+            role="hamgurger menu" aria-label="view the links to all the other pages"
+          >
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
               <path strokeLinecap="round" strokeLinejoin="round" d="M21.752 15.002A9.718 9.718 0 0118 15.75c-5.385 0-9.75-4.365-9.75-9.75 0-1.33.266-2.597.748-3.752A9.753 9.753 0 003 11.25C3 16.635 7.365 21 12.75 21a9.753 9.753 0 009.002-5.998z" />
             </svg>
