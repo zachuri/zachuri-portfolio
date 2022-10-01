a9e9bd67ebcdcc0ee8764d239e28c19b3f4b7739
zachuri <punsalang.zachary@gmail.com>
2022-09-30 20:31:58 -0700

    footer: accessibiilty checker-> viual issue (fixed bg/fg color don't have sufficent contrast ratio

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,22 +5,24 @@ import { FaLinkedin, FaGithub, FaTwitter } from "react-icons/fa";
 
 const Footer = () => {
 
+  const fillColor = "fill-[#999999] dark:fill-[#999999]"
+
   return (
     <LayoutMotion>
-      <div className="text-center opacity-25 text-sm p-4">
+      <div className="text-center text-[#999999] dark:text-[#999999] text-sm p-4">
         &copy; {new Date().getFullYear()} Zachary Punsalang. All Rights Reserved.
         <div className='flex flex-cols gap-5 justify-center'>
           <a href="https://www.linkedin.com/in/zachary-punsalang/">
-            <FaLinkedin className='fill-black dark:fill-white' />
+            <FaLinkedin className={fillColor} />
           </a>
           <a href="https://github.com/zachuri">
-            <FaGithub className='fill-black dark:fill-white' />
+            <FaGithub className={fillColor} />
           </a>
           <a href="https://twitter.com/zachurii">
-            <FaTwitter className='fill-black dark:fill-white' />
+            <FaTwitter className={fillColor} />
           </a>
           <a href="mailto:punsalang.zachary.work@gmial.com?body=">
-            <AiOutlineMail className='fill-black dark:fill-white' />
+            <AiOutlineMail className={fillColor} />
           </a>
         </div>
       </div>
