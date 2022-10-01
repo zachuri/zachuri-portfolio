a2739e80a092357f9daeb75495bb85f89d827551
zachuri <punsalang.zachary@gmail.com>
2022-10-01 00:46:05 -0700

    what: added keyboards to what page

--- src/pages/what.tsx
+++ src/pages/what.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import MainLayout from '../components/Layouts/Main';
 import LayoutMotion from '../components/Layouts/Motion';
 import Skills from '../components/Skills';
-import Skill from '../components/Skill';
+import Image from 'next/image';
 
 interface Props {
   children: React.ReactNode;
@@ -36,25 +36,45 @@ const What: React.FC = () => {
 
           <Container title={"Work Flow"}>
             <Container subTitle='Operating System'>
-              <p>- Arch Linux</p>
-              <p>- Dynamic Window Manager (DWM Suckless)</p>
+              <li>Arch Linux</li>
+              <li>Dynamic Window Manager (DWM Suckless)</li>
             </Container>
             <Container subTitle='Coding'>
-              <p>- Visual Studio Code</p>
-              <p>- Vim</p>
+              <li>Visual Studio Code</li>
+              <li>Vim</li>
             </Container>
           </Container>
 
           <Container title={"Personal Interests"}>
             <Container subTitle='Keyboards'>
-              <p className='pb-2'>During covid I fell in love with the keyboard community. As a developer I do
-                feel a bit more professional with my handy keyboard! My first every keeb I built
+              <p className='pb-2 -mx-4'>
+                During covid I fell in love with the keyboard community. As a developer I do
+                feel a bit more professional with my handy keyboard! My first ever keeb I built
                 is the Tofu 60%. It was my first baby and I adored it a lot. It got the job done and sounded
-                really great. However, I feel into the rabbit hole of trying to experience a luxury keyboard
-                with the layout I would like.
+                really great. However, I fell into the rabbit hole looking at luxury keyboards.
+                I found the JP-02 which is an arisu layout which is a slightly ergonmic. It looks really interesting
+                and typing on it is really comforatble. I would always encourage other developrs to obtain
+                their own keyboard with their own personality on it! Happy typing!
               </p>
-              <p>- JP-02 (Arisu)</p>
-              <p>- Tofu 60 (60%)</p>
+            </Container>
+            <Container subTitle='JP-02 (Arisu)'>
+              <Image
+                src="/assets/keyboards/jp-02.jpeg"
+                width="350"
+                height="550"
+                alt='jp-02 keyboard'
+                placeholder='blur'
+                blurDataURL='assets/keyboards/jp-02.jpeg'
+              />
+            </Container>
+            <Container subTitle='Tofu 60 (60%)'>
+              <Image
+                src="/assets/keyboards/tofu60.jpeg"
+                width="350"
+                height="500"
+                alt='tofu 60 keyboard'
+                blurDataURL='assets/keyboards/tofu60.jpeg'
+              />
             </Container>
           </Container>
         </div>
