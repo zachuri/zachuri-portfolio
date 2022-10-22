8f00bc68ddaa2ae6d912cf089d9f0585deeeae81
zachuri <punsalang.zachary@gmail.com>
2022-10-21 19:09:55 -0700

    be-fit: added page and components

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,14 +1,39 @@
-import React from 'react'
-import Project from './Project'
+import React from 'react';
+import Project from './Project';
 
 const Projects: React.FC = () => {
   return (
     <div className="mt-5 grid grid-cols-1 md:grid-cols-2 gap-6 ">
-      <Project id={"twitch-clone"} title={"Twitch Clone"} desc={"Clone of Twitch using next.js and tailwind.css to help my understanding of design"} />
-      <Project id={"ecommerce-movie"} title={"Ecommerce Movie"} desc={"Full Stack ecommerce movie shop where users are able to buy movies"} />
-      <Project id={"diversity-hotline"} title={"Diversity Hotline"} desc={"Hotline for individuals to discuss topics regarding diversity, equity, inclusion, and discrimination"} />
+      <Project
+        id={'be-fit'}
+        title={'BeFit'}
+        desc={
+          'Full Stack Fitness tracker to help me an my brother keep track of our fitness progress'
+        }
+      />
+      <Project
+        id={'twitch-clone'}
+        title={'Twitch Clone'}
+        desc={
+          'Clone of Twitch using next.js and tailwind.css to help my understanding of design'
+        }
+      />
+      <Project
+        id={'ecommerce-movie'}
+        title={'Ecommerce Movie'}
+        desc={
+          'Full Stack ecommerce movie shop where users are able to buy movies'
+        }
+      />
+      <Project
+        id={'diversity-hotline'}
+        title={'Diversity Hotline'}
+        desc={
+          'Hotline for individuals to discuss topics regarding diversity, equity, inclusion, and discrimination'
+        }
+      />
     </div>
-  )
-}
+  );
+};
 
-export default Projects
+export default Projects;
