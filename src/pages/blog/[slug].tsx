08ae687393cb38b96e03ad307b4193092af491e1
zachuri <punsalang.zachary@gmail.com>
2022-11-02 04:02:16 -0700

    seo: changed format for blog posts:

--- src/pages/blog/[slug].tsx
+++ src/pages/blog/[slug].tsx
@@ -37,7 +37,7 @@ type PostPageProps = {
 
 const PostPage = ({ source, frontMatter }: PostPageProps): JSX.Element => {
   const customMeta: MetaProps = {
-    title: `${frontMatter.title} - ZACHURI | DEV`,
+    title: `ZACHURI | Blog - ${frontMatter.title}`,
     description: frontMatter.description,
     // image: `${WEBSITE_HOST_URL}${frontMatter.image}`,
     date: frontMatter.date,
