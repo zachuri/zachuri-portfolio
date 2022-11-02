c200e902bab31e4ed6fbb72c46fcdf134d31f0ea
zachuri <punsalang.zachary@gmail.com>
2022-11-02 01:46:13 -0700

    index: added seo explaining its my website

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,7 +7,7 @@ import Who from '../components/Who';
 const url = 'https://zachuri.com';
 const title = 'Home';
 const description =
-  "Hola I'm ZACHURI. I'm a Computer and Information Science graduate from the University of California, Irvine. What inspires me to code is being able to build valuable projects that can help individuals with their lives, my curiosity on wanting to know how things are built from the ground up, and how one is able to express their ideas through code. I am a developer that is willing to learn new ideas and technologies.";
+  "Zachary Punsalang Website - Hola I'm ZACHURI. I'm a Computer and Information Science graduate from the University of California, Irvine. What inspires me to code is being able to build valuable projects that can help individuals with their lives, my curiosity on wanting to know how things are built from the ground up, and how one is able to express their ideas through code. I am a developer that is willing to learn new ideas and technologies.";
 
 const Home: NextPage = () => {
   return (
