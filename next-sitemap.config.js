f2ceb6972bbf58c3997803334679227f0dda4b4f
zachuri <punsalang.zachary@gmail.com>
2022-11-03 02:28:31 -0700

    next-sitemap.config: added policies for robotsTxt

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,5 +1,12 @@
 module.exports = {
   siteUrl: 'https://zachuri.com',
-  generateRobotsTxt: true // (optional)
+  generateRobotsTxt: true, // (optional)
+  robotsTxtOptions: {
+    policies: [
+      { userAgent: '*', disallow: '/contact' },
+      { userAgent: '*', allow: '/' }
+    ]
+  },
+  exclude: ['/contact']
   // ...other options
 };
