ee29bdcf5052204c860306676a8c370b74b2c311
ZACHURI <punsalang.zachary@gmail.com>
2022-11-13 23:50:37 -0800

    color-way: added links to each link

--- src/components/Links.tsx
+++ src/components/Links.tsx
@@ -6,18 +6,22 @@ interface Props {
 	children: React.ReactNode;
 	name: string;
 	desc: string;
+	link: string;
+	label: string;
 }
 
-const Link: React.FC<Props> = ({ children, name, desc }) => {
+const Link: React.FC<Props> = ({ children, name, desc, link, label }) => {
 	return (
 		<>
-			<div className='mx-3 mb-4 items-center bg-[#FEF6F8] hover:bg-[#ded6d9] dark:bg-[#d0caca] dark:hover:bg-[#7c7878] transition text-[#77778b] dark:text-[#292929] p-3 rounded-xl grid grid-cols-5 hover '>
-				<div className='ml-5 items-center'>{children}</div>
-				<div className='col-span-3 flex flex-col items-center justify-center'>
-					<h2 className='text-lg'>{name}</h2>
-					<p className='text-xs'>{desc}</p>
+			<a href={link} aria-label={label}>
+				<div className='mx-3 mb-4 items-center bg-[#FEF6F8] hover:bg-[#ded6d9] dark:bg-[#d0caca] dark:hover:bg-[#7c7878] transition text-[#77778b] dark:text-[#292929] p-3 rounded-xl grid grid-cols-5 hover '>
+					<div className='ml-5 items-center'>{children}</div>
+					<div className='col-span-3 flex flex-col items-center justify-center'>
+						<h2 className='text-lg'>{name}</h2>
+						<p className='text-xs'>{desc}</p>
+					</div>
 				</div>
-			</div>
+			</a>
 		</>
 	);
 };
@@ -27,19 +31,35 @@ const Links: React.FC = () => {
 		<>
 			{/* Links */}
 			<div className='flex flex-col w-full mb-5'>
-				<Link name='Linkden' desc='I might connect'>
+				<Link
+					name='Github'
+					desc='My spaghetti codes'
+					link='https://github.com/zachuri'
+					label='link to github'>
+					<FaGithub size={30} className='fill-[#77778b] dark:fill-[#292929]' />
+				</Link>
+				<Link
+					name='Linkden'
+					desc='I might connect'
+					link='https://www.linkedin.com/in/zachary-punsalang/'
+					label='link to linkden'>
 					<FaLinkedin
 						size={30}
 						className='fill-[#77778b] dark:fill-[#292929]'
 					/>
 				</Link>
-				<Link name='Github' desc='My spaghetti codes'>
-					<FaGithub size={30} className='fill-[#77778b] dark:fill-[#292929]' />
-				</Link>
-				<Link name='Twitter' desc='FLY'>
+				<Link
+					name='Twitter'
+					desc='FLY'
+					link='https://twitter.com/zachurii'
+					label='link to twitter'>
 					<FaTwitter size={30} className='fill-[#77778b] dark:fill-[#292929]' />
 				</Link>
-				<Link name='Mail' desc='Ready to Responsd'>
+				<Link
+					name='Mail'
+					desc='Ready to Responsd'
+					link='mailto:punsalang.zachary.work@gmail.com?body='
+					label='link to mail'>
 					<AiOutlineMail
 						size={30}
 						className='fill-[#77778b] dark:fill-[#292929]'
