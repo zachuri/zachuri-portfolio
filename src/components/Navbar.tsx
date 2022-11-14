261789a9e06b2df23b393933b22ff4f1668269a2
zachuri <punsalang.zachary@gmail.com>
2022-11-14 00:19:29 -0800

    navbar: darkToggle -> added new icon

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -26,6 +26,8 @@ const Navbar: React.FC = () => {
     theme === 'dark' ? false : true
   );
 
+  const [icon, setIcon] = useState<boolean>(true);
+
   // console.log(theme);
   // console.log(darkToggle);
 
@@ -34,6 +36,8 @@ const Navbar: React.FC = () => {
   }, [darkToggle]);
 
   function handleTheme() {
+    setIcon(curr => (curr === false ? true : false));
+
     if (theme === 'dark') {
       setTheme('light');
       setDarkToggle(false);
@@ -83,20 +87,37 @@ const Navbar: React.FC = () => {
             role="toggle"
             aria-label="toggle between light and dark mode"
           >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              strokeWidth={1.5}
-              stroke="currentColor"
-              className="w-6 h-6"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M21.752 15.002A9.718 9.718 0 0118 15.75c-5.385 0-9.75-4.365-9.75-9.75 0-1.33.266-2.597.748-3.752A9.753 9.753 0 003 11.25C3 16.635 7.365 21 12.75 21a9.753 9.753 0 009.002-5.998z"
-              />
-            </svg>
+            {icon ? (
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className="w-6 h-6"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M12 3v2.25m6.364.386l-1.591 1.591M21 12h-2.25m-.386 6.364l-1.591-1.591M12 18.75V21m-4.773-4.227l-1.591 1.591M5.25 12H3m4.227-4.773L5.636 5.636M15.75 12a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0z"
+                />
+              </svg>
+            ) : (
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className="w-6 h-6"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M21.752 15.002A9.718 9.718 0 0118 15.75c-5.385 0-9.75-4.365-9.75-9.75 0-1.33.266-2.597.748-3.752A9.753 9.753 0 003 11.25C3 16.635 7.365 21 12.75 21a9.753 9.753 0 009.002-5.998z"
+                />
+              </svg>
+            )}
           </button>
           <Link href="/contact">CONTACT</Link>
         </div>
