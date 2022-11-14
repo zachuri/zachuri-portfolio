155ab0abe2deaee10b6ef0f759dab649ea5d0faa
zachuri <punsalang.zachary@gmail.com>
2022-11-14 06:13:50 -0800

    theme: attribute -> changed to class

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -10,11 +10,7 @@ import SEO from '../../next-seo.config';
 const MyApp: AppType = ({ Component, pageProps }) => {
   // return <Component {...pageProps} />
   return (
-    <ThemeProvider
-      attribute="className"
-      defaultTheme="light"
-      storageKey="theme"
-    >
+    <ThemeProvider attribute="class" defaultTheme="light" storageKey="theme">
       <DefaultSeo {...SEO} />
       <Navbar />
       <AnimatePresence exitBeforeEnter initial={true}>
