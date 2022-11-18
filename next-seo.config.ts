9f993687eedeba765d9205f72e7c78c15ae4afba
zachuri <punsalang.zachary@gmail.com>
2022-11-18 03:46:08 -0800

    updated seo config/ added site-preview

--- next-seo.config.ts
+++ next-seo.config.ts
@@ -1,4 +1,4 @@
-const title = 'ZACHURI | WEBSITE';
+const title = 'ZACHURI | Website';
 const description =
   "Hola I'm ZACHURI. I'm a Computer and Information Science graduate from the University of California, Irvine. What inspires me to code is being able to build valuable projects that can help individuals with their lives, my curiosity on wanting to know how things are built from the ground up, and how one is able to express their ideas through code. I am a developer that is willing to learn new ideas and technologies.";
 
@@ -14,7 +14,7 @@ const SEO = {
     description,
     images: [
       {
-        url: 'https://zachuri.com/site-preview.png',
+        url: 'https://www.zachuri.com/site-preview.png',
         alt: title
       }
     ]
