28874b517bb3a2784810e7c30129b99ee3bde49d
zachuri <punsalang.zachary@gmail.com>
2023-04-07 03:33:25 -0700

    skills: added a few more

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -1,31 +1,35 @@
-import React from 'react'
-import Skill from './Skill'
+import React from 'react';
+import Skill from './Skill';
 
 const names = [
-  { id: "reactjs", skillName: 'React.js' },
-  { id: "nextjs", skillName: 'Next.js' },
-  { id: "reactnative", skillName: 'React Native' },
-  { id: "tailwindd", skillName: 'Tailwind CSS' },
-  { id: "java", skillName: 'Java' },
-  { id: "c/c++", skillName: 'C/C++' },
-  { id: "javascript", skillName: 'JavaScript' },
-  { id: "python", skillName: 'Python' },
-  { id: "springboot", skillName: 'SpringBoot' },
-  { id: "mysql", skillName: 'MySQL' },
-  { id: "trpc", skillName: 'TRPC' },
-  { id: "htmlcss", skillName: 'HTML/CSS' },
-  { id: "vim", skillName: 'VIM' },
-  { id: "git", skillName: 'Git' },
-]
+  { id: 'reactjs', skillName: 'React.js' },
+  { id: 'nextjs', skillName: 'Next.js' },
+  { id: 'reactnative', skillName: 'React Native' },
+  { id: 'tailwindd', skillName: 'Tailwind CSS' },
+  { id: 'java', skillName: 'Java' },
+  { id: 'c/c++', skillName: 'C/C++' },
+  { id: 'typescript', skillName: 'Typecript' },
+  { id: 'javascript', skillName: 'JavaScript' },
+  { id: 'python', skillName: 'Python' },
+  { id: 'springboot', skillName: 'Spring Boot' },
+  { id: 'supabase', skillName: 'Supabase' },
+  { id: 'Firebase', skillName: 'Firebase' },
+  { id: 'mysql', skillName: 'MySQL' },
+  { id: 'postgres', skillName: 'Postgres' },
+  { id: 'trpc', skillName: 'tRPC' },
+  { id: 'htmlcss', skillName: 'HTML/CSS' },
+  { id: 'vim', skillName: 'Vim' },
+  { id: 'git', skillName: 'Git' }
+];
 
 const Skills = () => {
   return (
     <>
-      {names.map((name) => (
+      {names.map(name => (
         <Skill key={name.id} name={name.skillName} />
       ))}
     </>
-  )
-}
+  );
+};
 
-export default Skills
+export default Skills;
