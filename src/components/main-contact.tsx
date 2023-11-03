d4363243f11d7d92ded2a5806db631e0984cf56f
zachuri <punsalang.zachary@gmail.com>
2023-11-03 02:31:55 -0700

    main: updated size of card on smaller screens

--- src/components/main-contact.tsx
+++ src/components/main-contact.tsx
@@ -8,26 +8,26 @@ interface Props {
 
 const MainContact: React.FC<Props> = ({ items }) => {
   return (
-    <div className="mx-auto grid justify-center gap-4 md:max-w-[64rem] grid-cols-2 lg:grid-cols-3">
+    <div className="mx-auto grid justify-center gap-4 md:max-w-[64rem] sm:grid-cols-2 lg:grid-cols-3">
       {items?.map(item => {
         const Icon = Icons[item.icon || 'arrowRight'];
         return (
           <div
             key={item.name}
-            className="relative overflow-hidden rounded-lg border bg-background p-2"
-            style={{ width: '100%' }}
+            className="relative overflow-hidden rounded-lg border bg-background md:p-2"
+            style={{ width: '100%' }} // Set the width to 100%
           >
-            <div className="flex flex-col justify-between rounded-md p-6">
+            <div className="flex h-[100x] flex-col justify-between rounded-md p-4 md:p-6">
               <div className="space-y-2">
-                <div className="flex items-center">
+                <div className="grid grid-cols-6 items-center">
                   {/* @ts-ignore */}
                   <Icon
                     className="mr-2 h-6 w-6 md:h-7 md:w-7"
                     strokeWidth={1.5}
                   />
-                  <div>
+                  <div className="col-span-5">
                     <h3 className="font-bold">{item.name}</h3>
-                    <p className="text-sm text-muted-foreground max-sm:hidden">
+                    <p className="text-sm text-muted-foreground">
                       {item.description}
                     </p>
                   </div>
