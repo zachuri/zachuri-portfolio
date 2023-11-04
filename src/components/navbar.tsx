0530fd5050827da43e0c9eb851784c383ef20132
zachuri <punsalang.zachary@gmail.com>
2023-11-04 13:57:01 -0700

    navbar: created reusable navbar

---
+++ src/components/navbar.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { MainNav } from './main-nav';
+import { ModeToggle } from './mode-toggle';
+import { mainConfig } from '@/config/main';
+
+const Navbar = () => {
+  return (
+    <header className="sticky top-0 z-40 bg-opacity-20 backdrop-blur-lg drop-shadow-lg">
+      <div className="container flex h-16 items-center justify-between py-4">
+        <MainNav items={mainConfig.mainNav} />
+        <ModeToggle />
+      </div>
+    </header>
+  );
+};
+
+export default Navbar;
