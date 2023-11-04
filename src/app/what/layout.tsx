af5bc2cd56d6efb80840813bb772a57d0b259fbd
zachuri <punsalang.zachary@gmail.com>
2023-11-04 13:57:17 -0700

    navbar: added navbar to layout of main and what

--- src/app/what/layout.tsx
+++ src/app/what/layout.tsx
@@ -1,11 +1,15 @@
-import { SiteFooter } from '@/components/site-footer';
+import { MainNav } from '@/components/main-nav';
+import { ModeToggle } from '@/components/mode-toggle';
 import Navbar from '@/components/navbar';
+import { SiteFooter } from '@/components/site-footer';
+import { mainConfig } from '@/config/main';
+import React from 'react';
 
 interface MainLayoutProps {
   children: React.ReactNode;
 }
 
-export default async function MainLayout({ children }: MainLayoutProps) {
+export default async function Layout({ children }: MainLayoutProps) {
   return (
     <div className="flex min-h-screen flex-col space-y-6">
       <Navbar />
