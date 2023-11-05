35a1a3ca23836d6c09848e4b1a5006ce6be00e3e
ZACHURI <punsalang.zachary@gmail.com>
2023-11-05 13:12:36 -0800

    updated position to be necessary

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,20 +1,19 @@
-import { MainNav } from './main-nav';
-import { Icons } from './ui/icons';
+import { MainNav } from "./main-nav";
+import { Icons } from "./ui/icons";
 
 const Navbar = () => {
-  return (
-    <>
-      <header className="sticky top-0 z-40 bg-opacity-20 backdrop-blur-lg drop-shadow-lg">
-        <div className="p-5 flex justify-center items-center">
-          <div className="flex flex-row p-2 gap-x-6 rounded-xl bg-primary text-primary-foreground">
-            <Icons.logo />
-            <MainNav />
-            <Icons.logo />
-          </div>
-        </div>
-      </header>
-    </>
-  );
+	return (
+		<>
+			<header className='sticky top-0 z-40 bg-opacity-20 backdrop-blur-lg drop-shadow-lg'>
+				<div className='p-5 flex justify-center items-center'>
+					<div className='flex flex-row p-2 gap-x-6 rounded-xl bg-primary text-primary-foreground'>
+						<Icons.logo />
+						<MainNav />
+					</div>
+				</div>
+			</header>
+		</>
+	);
 };
 
 export default Navbar;
