436edcd7bad1686b6f14dd661ff806382ca584c6
ZACHURI <punsalang.zachary@gmail.com>
2023-11-05 12:53:50 -0800

    navbar: updated positions (need to fix later)

--- src/config/main.ts
+++ src/config/main.ts
@@ -1,30 +1,30 @@
-import { MainConfig } from '@/types';
+import { MainConfig } from "@/types";
 
 export const mainConfig: MainConfig = {
-  mainNav: [
-    {
-      title: 'Who',
-      href: '/',
-      position: -7
-    },
-    {
-      title: 'What',
-      href: '/what',
-      position: 47
-    },
-    {
-      title: 'Works',
-      href: '/works',
-      position: 106
-    },
-    {
-      title: 'Blog',
-      href: '/blog',
-      position: 172
-    }
-    // {
-    //   title: 'Contact',
-    //   href: '/contact'
-    // }
-  ]
+	mainNav: [
+		{
+			title: "Who",
+			href: "/",
+			position: -7,
+		},
+		{
+			title: "What",
+			href: "/what",
+			position: 50.5,
+		},
+		{
+			title: "Works",
+			href: "/works",
+			position: 111,
+		},
+		{
+			title: "Blog",
+			href: "/blog",
+			position: 177,
+		},
+		// {
+		//   title: 'Contact',
+		//   href: '/contact'
+		// }
+	],
 };
