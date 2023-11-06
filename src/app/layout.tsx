2f7bdc522757e2f81274dc89abf16f2752458c97
ZACHURI <punsalang.zachary@gmail.com>
2023-11-05 18:48:08 -0800

    layout: fix layout spacing for mobile screen

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,41 +1,40 @@
-import type { Metadata } from 'next';
-import { Inter, Roboto } from 'next/font/google';
-import './globals.css';
-import { ThemeProvider } from '@/components/theme-proivder';
-import { TailwindIndicator } from '@/components/tailwind-indicator';
-import { Toaster } from '@/components/ui/toaster';
-import { MainNav } from '@/components/main-nav';
-import { mainConfig } from '@/config/main';
-import Navbar from '@/components/navbar';
+import type { Metadata } from "next";
+import { Inter, Roboto } from "next/font/google";
+import "./globals.css";
+import { ThemeProvider } from "@/components/theme-proivder";
+import { TailwindIndicator } from "@/components/tailwind-indicator";
+import { Toaster } from "@/components/ui/toaster";
+import { MainNav } from "@/components/main-nav";
+import { mainConfig } from "@/config/main";
+import Navbar from "@/components/navbar";
 
-const font = Inter({ subsets: ['latin'] });
+const font = Inter({ subsets: ["latin"] });
 // const font = Roboto({ subsets: ['latin'], weight: ['400'] });
 
 export const metadata: Metadata = {
-  title: 'Zachary Punsalang ',
-  description: 'Personal Website'
+	title: "Zachary Punsalang ",
+	description: "Personal Website",
 };
 
 export default function RootLayout({
-  children
+	children,
 }: {
-  children: React.ReactNode;
+	children: React.ReactNode;
 }) {
-  return (
-    <html lang="en" suppressHydrationWarning>
-      <body className={`${font.className}`}>
-        <ThemeProvider
-          attribute="class"
-          defaultTheme="system"
-          enableSystem
-          disableTransitionOnChange
-        >
-          <Navbar />
-          {children}
-          <TailwindIndicator />
-          <Toaster />
-        </ThemeProvider>
-      </body>
-    </html>
-  );
+	return (
+		<html lang='en' suppressHydrationWarning>
+			<body className={`${font.className}`}>
+				<ThemeProvider
+					attribute='class'
+					defaultTheme='system'
+					enableSystem
+					disableTransitionOnChange>
+					<Navbar />
+					{children}
+					<TailwindIndicator />
+					<Toaster />
+				</ThemeProvider>
+			</body>
+		</html>
+	);
 }
