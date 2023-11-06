9aed49b7a918fc8679b922f22435534858c80948
ZACHURI <punsalang.zachary@gmail.com>
2023-11-05 18:44:34 -0800

    mode-toggle: changed button variant to ghost

--- src/components/mode-toggle.tsx
+++ src/components/mode-toggle.tsx
@@ -11,7 +11,7 @@ import {
 	DropdownMenuItem,
 	DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
-import { motion } from 'framer-motion';
+import { motion } from "framer-motion";
 
 export function ModeToggle() {
 	const { theme, setTheme } = useTheme();
@@ -19,21 +19,19 @@ export function ModeToggle() {
 	return (
 		<DropdownMenu>
 			<DropdownMenuTrigger asChild>
-      <motion.div
-        style={{ display: 'inline-block' }}
-        key={theme}
-        initial={{ y: -20, opacity: 0 }}
-        animate={{ y: 0, opacity: 1 }}
-        exit={{ y: 20, opacity: 0 }}
-        transition={{ duration: 0.2 }}
-      >
-				<Button size='sm'>
-					<Sun className='h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0' />
-					<Moon className='absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100' />
-					<span className='sr-only'>Toggle theme</span>
-				</Button>
-
-      </motion.div>
+				<motion.div
+					style={{ display: "inline-block" }}
+					key={theme}
+					initial={{ y: -20, opacity: 0 }}
+					animate={{ y: 0, opacity: 1 }}
+					exit={{ y: 20, opacity: 0 }}
+					transition={{ duration: 0.2 }}>
+					<Button variant='ghost' size='sm'>
+						<Sun className='h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0' />
+						<Moon className='absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100' />
+						<span className='sr-only'>Toggle theme</span>
+					</Button>
+				</motion.div>
 			</DropdownMenuTrigger>
 			<DropdownMenuContent align='end'>
 				<DropdownMenuItem onClick={() => setTheme("light")}>
