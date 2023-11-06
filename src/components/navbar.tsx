c20cddecaf99f893f5e96cf832b2464332b53b77
zachuri <punsalang.zachary@gmail.com>
2023-11-05 21:31:26 -0800

    mode-toggle: update to add props to pass/ changed global radius/ updated main positions

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,27 +1,27 @@
-import Link from "next/link";
-import { MainNav } from "./main-nav";
-import { ModeToggle } from "./mode-toggle";
-import { Button } from "./ui/button";
-import { Icons } from "./ui/icons";
+import Link from 'next/link';
+import { MainNav } from './main-nav';
+import { ModeToggle } from './mode-toggle';
+import { Button } from './ui/button';
+import { Icons } from './ui/icons';
 
 const Navbar = () => {
-	return (
-		<>
-			<header className='sticky top-0 z-40'>
-				<div className='p-5 flex justify-center items-center'>
-					<div className='flex flex-row items-center justify-center p-2 gap-x-3 rounded-xl bg-primary text-primary-foreground'>
-						<Button size={"sm"}>
-							<Link href='/'>
-								<Icons.logo />
-							</Link>
-						</Button>
-						<MainNav />
-						<ModeToggle />
-					</div>
-				</div>
-			</header>
-		</>
-	);
+  return (
+    <>
+      <header className="sticky top-0 z-40">
+        <div className="p-5 flex justify-center items-center">
+          <div className="flex flex-row items-center justify-center p-1 gap-x-3 rounded-xl bg-primary text-primary-foreground">
+            <Button size={'sm'}>
+              <Link href="/">
+                <Icons.logo />
+              </Link>
+            </Button>
+            <MainNav />
+            <ModeToggle variant={'ghost'} size={'sm'} />
+          </div>
+        </div>
+      </header>
+    </>
+  );
 };
 
 export default Navbar;
--- src/config/main.ts
+++ src/config/main.ts
@@ -1,30 +1,30 @@
-import { MainConfig } from "@/types";
+import { MainConfig } from '@/types';
 
 export const mainConfig: MainConfig = {
-	mainNav: [
-		{
-			title: "Who",
-			href: "/",
-			position: -7,
-		},
-		{
-			title: "What",
-			href: "/what",
-			position: 50.5,
-		},
-		{
-			title: "Works",
-			href: "/works",
-			position: 111,
-		},
-		{
-			title: "Blog",
-			href: "/blog",
-			position: 177,
-		},
-		// {
-		//   title: 'Contact',
-		//   href: '/contact'
-		// }
-	],
+  mainNav: [
+    {
+      title: 'Who',
+      href: '/',
+      position: -11.2
+    },
+    {
+      title: 'What',
+      href: '/what',
+      position: 43.6
+    },
+    {
+      title: 'Works',
+      href: '/works',
+      position: 102.9
+    },
+    {
+      title: 'Blog',
+      href: '/blog',
+      position: 168.6
+    }
+    // {
+    //   title: 'Contact',
+    //   href: '/contact'
+    // }
+  ]
 };
