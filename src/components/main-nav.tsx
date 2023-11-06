2f3abdfa6c01ff49b96d5371f4e681da28c132f0
zachuri <punsalang.zachary@gmail.com>
2023-11-05 21:30:28 -0800

    main-nav: updated the padding and width of the navbar

--- src/components/main-nav.tsx
+++ src/components/main-nav.tsx
@@ -1,45 +1,46 @@
-"use client";
+'use client';
 
-import React, { useEffect, useState } from "react";
-import { AnimatePresence, motion } from "framer-motion";
-import { mainConfig } from "@/config/main";
-import { usePathname } from "next/navigation";
-import Link from "next/link";
+import React, { useEffect, useState } from 'react';
+import { AnimatePresence, motion } from 'framer-motion';
+import { mainConfig } from '@/config/main';
+import { usePathname } from 'next/navigation';
+import Link from 'next/link';
 
 export function MainNav() {
-	const path = usePathname();
+  const path = usePathname();
 
-	const items = mainConfig.mainNav;
+  const items = mainConfig.mainNav;
 
-	const defaultActiveIndex = items.findIndex(item => item.href === path);
+  const defaultActiveIndex = items.findIndex(item => item.href === path);
 
-	const [activeIndex, setActiveIndex] = useState(defaultActiveIndex);
+  const [activeIndex, setActiveIndex] = useState(defaultActiveIndex);
 
-	useEffect(() => {
-		const defaultActiveIndex = items.findIndex(item => item.href === path);
-		setActiveIndex(defaultActiveIndex);
-	}, [path]);
+  useEffect(() => {
+    const defaultActiveIndex = items.findIndex(item => item.href === path);
+    setActiveIndex(defaultActiveIndex);
+  }, [path]);
 
-	return (
-		<div className='flex items-center gap-x-7 relative'>
-			{items.map((item, index) => (
-				<Link key={index} href={item.href} className='text-sm font-medium'>
-					{item.title}
-				</Link>
-			))}
-			<AnimatePresence mode='wait' initial={false}>
-				<motion.div
-					className='bg-background absolute text-sm font-medium text-primary rounded-[8px] p-2'
-					initial={{ opacity: 0, x: items[defaultActiveIndex]?.position ?? 0 }}
-					animate={{
-						opacity: 1,
-						x: items[activeIndex]?.position ?? 0,
-						transition: { duration: 0.3 },
-					}}
-					exit={{ opacity: 0, x: items[activeIndex]?.position ?? 0 }}>
-					{items[activeIndex]?.title}
-				</motion.div>
-			</AnimatePresence>
-		</div>
-	);
+  return (
+    <div className="flex items-center gap-x-7 relative">
+      {items.map((item, index) => (
+        <Link key={index} href={item.href} className="text-sm font-medium">
+          {item.title}
+        </Link>
+      ))}
+      <AnimatePresence mode="wait" initial={false}>
+        <motion.div
+          className="bg-background absolute text-sm font-medium text-primary rounded-lg p-1 px-3"
+          initial={{ opacity: 0, x: items[defaultActiveIndex]?.position ?? 0 }}
+          animate={{
+            opacity: 1,
+            x: items[activeIndex]?.position ?? 0,
+            transition: { duration: 0.3 }
+          }}
+          exit={{ opacity: 0, x: items[activeIndex]?.position ?? 0 }}
+        >
+          {items[activeIndex]?.title}
+        </motion.div>
+      </AnimatePresence>
+    </div>
+  );
 }
