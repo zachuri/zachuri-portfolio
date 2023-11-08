3b4b1bd776a6a410f7e56518b56068cb04e41d6b
zachuri <punsalang.zachary@gmail.com>
2023-11-07 20:38:24 -0800

    navbar: working navbar with dynamic box and underline

--- src/components/main-nav.tsx
+++ src/components/main-nav.tsx
@@ -1,10 +1,17 @@
 'use client';
 
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { AnimatePresence, motion } from 'framer-motion';
 import { mainConfig } from '@/config/main';
-import { usePathname } from 'next/navigation';
+import { usePathname, useRouter } from 'next/navigation';
 import Link from 'next/link';
+import { cn } from '@/lib/utils';
+
+const transition = {
+  type: 'tween',
+  ease: 'easeOut',
+  duration: 0.15
+};
 
 export function MainNav() {
   const path = usePathname();
@@ -13,34 +20,112 @@ export function MainNav() {
 
   const defaultActiveIndex = items.findIndex(item => item.href === path);
 
-  const [activeIndex, setActiveIndex] = useState(defaultActiveIndex);
+  const [activeIndex, setActiveIndex] = useState<number>(defaultActiveIndex);
+
+  // useEffect(() => {
+  //   const defaultActiveIndex = items.findIndex(item => item.href === path);
+  //   setActiveIndex(defaultActiveIndex);
+  // }, [path]);
+
+  const [buttonRefs, setButtonRefs] = useState<Array<HTMLButtonElement | null>>(
+    []
+  );
 
   useEffect(() => {
-    const defaultActiveIndex = items.findIndex(item => item.href === path);
-    setActiveIndex(defaultActiveIndex);
-  }, [path]);
+    setButtonRefs(prev => prev.slice(0, items.length));
+  }, [items.length]);
+
+  const navRef = useRef<HTMLDivElement>(null);
+  const navRect = navRef.current?.getBoundingClientRect();
+
+  const selectedRect = buttonRefs[activeIndex]?.getBoundingClientRect();
+
+  const [hoveredTabIndex, setHoveredTabIndex] = useState<number | null>(null);
+  const hoveredRect =
+    buttonRefs[hoveredTabIndex ?? -1]?.getBoundingClientRect();
 
   return (
-    <div className="flex items-center gap-x-7 relative">
-      <AnimatePresence mode="wait" initial={false}>
-        <motion.div
-          className="bg-background absolute text-sm font-medium text-primary rounded-lg p-1 px-3"
-          initial={{ opacity: 0, x: items[defaultActiveIndex]?.position ?? 0 }}
-          animate={{
-            opacity: 1,
-            x: items[activeIndex]?.position ?? 0,
-            transition: { duration: 0.3 }
-          }}
-          exit={{ opacity: 0, x: items[activeIndex]?.position ?? 0 }}
-        >
-          {items[activeIndex]?.title}
-        </motion.div>
+    <section
+      ref={navRef}
+      onPointerLeave={e => setHoveredTabIndex(null)}
+      className="flex items-center justify-center relative"
+    >
+      {items.map((item, index) => {
+        const isActive = hoveredTabIndex === index || activeIndex === index;
+
+        return (
+          <motion.button
+            key={index}
+            className={cn(
+              'font-medium z-20 text-sm relative rounded-lg flex items-center h-8 px-4 bg-transparent cursor-pointer select-none transition-colors',
+              {
+                'text-background': !isActive, // Default color for non-active tabs
+                'text-primary': isActive // Color for active tabs
+              }
+            )}
+            ref={el => (buttonRefs[index] = el)}
+            onPointerEnter={() => {
+              setHoveredTabIndex(index);
+            }}
+            onFocus={() => {
+              setHoveredTabIndex(index);
+            }}
+            onClick={() => {
+              setActiveIndex(index);
+              console.log('HELLLO');
+            }}
+          >
+            <Link href={item.href}>{item.title}</Link>
+          </motion.button>
+        );
+      })}
+
+      {/* Underline */}
+      <AnimatePresence>
+        {hoveredRect && navRect && (
+          <motion.div
+            className={
+              'absolute z-10 bottom-0 left-0.5 h-[3px] bg-background/90'
+            }
+            initial={false}
+            animate={{
+              width: hoveredRect.width * 0.8,
+              x: `calc(${hoveredRect.left - navRect.left}px + 10%)`,
+              opacity: 1
+            }}
+            transition={transition}
+          />
+        )}
       </AnimatePresence>
-      {items.map((item, index) => (
-        <Link key={index} href={item.href} className="text-sm font-medium">
-          {item.title}
-        </Link>
-      ))}
-    </div>
+
+      {/* Box */}
+      {selectedRect && navRect && (
+        <motion.div
+          className="absolute p-1 mb-1 z-10 top-0 left-0 rounded-md bg-background"
+          initial={{
+            x: selectedRect.left - navRect.left,
+            y: selectedRect.top - navRect.top,
+            width: selectedRect.width,
+            height: selectedRect.height,
+            opacity: 0
+          }}
+          animate={{
+            x: selectedRect.left - navRect.left,
+            y: selectedRect.top - navRect.top,
+            width: selectedRect.width,
+            height: selectedRect.height,
+            opacity: 1
+          }}
+          exit={{
+            x: selectedRect.left - navRect.left,
+            y: selectedRect.top - navRect.top,
+            width: selectedRect.width,
+            height: selectedRect.height,
+            opacity: 0
+          }}
+          transition={transition}
+        />
+      )}
+    </section>
   );
 }
