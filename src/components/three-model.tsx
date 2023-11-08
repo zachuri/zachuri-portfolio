e326df041827158629f9351617ed326543ed0247
zachuri <punsalang.zachary@gmail.com>
2023-11-08 01:27:23 -0800

    three-model: updated code to be cleaner and understandable

--- src/components/three-model.tsx
+++ src/components/three-model.tsx
@@ -2,17 +2,18 @@
 
 import { useRef } from 'react';
 import { Canvas, useFrame, useLoader } from '@react-three/fiber';
-import { OrbitControls } from '@react-three/drei';
+import { OrbitControls, useGLTF } from '@react-three/drei';
 // @ts-ignore
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
 import { Mesh, Scene } from 'three';
 import { DRACOLoader } from 'three/examples/jsm/loaders/DRACOLoader.js';
 
-function MeshComponent() {
+function ModelMesh({ ...props }) {
   const fileUrl = '/zachuri-person.glb';
-  const mesh = useRef<Mesh>(null!);
+  const meshRef = useRef<Mesh>(null!);
 
   // const gltf = useLoader(GLTFLoader, fileUrl);
+
   const gltf = useLoader(GLTFLoader, fileUrl, loader => {
     // Load DRACOLoader
     const dracoLoader = new DRACOLoader();
@@ -23,13 +24,19 @@ function MeshComponent() {
   });
 
   useFrame(({ camera }) => {
-    mesh.current.rotation.y += 0.002;
+    meshRef.current.rotation.y += 0.002;
 
     camera.lookAt(0, 0, 0); // Look at the center of the model
   });
 
   return (
-    <mesh ref={mesh}>
+    <mesh {...props} ref={meshRef} castShadow receiveShadow>
+      <directionalLight
+        castShadow
+        position={[2.5, 8, 5]}
+        shadow-mapSize={[1024, 1024]}
+      />
+      <meshStandardMaterial />
       <primitive object={gltf.scene} />
     </mesh>
   );
@@ -39,11 +46,8 @@ export function ThreeModel() {
   return (
     <Canvas>
       <OrbitControls />
-      <ambientLight />
-      <directionalLight castShadow />
-      <mesh castShadow receiveShadow />
-      <pointLight position={[5, 10, 5]} />
-      <MeshComponent />
+      <ambientLight/>
+      <ModelMesh />
     </Canvas>
   );
 }
