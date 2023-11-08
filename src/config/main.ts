4b09ac5b525211a05683b15d3b8ef6a198bcac9a
zachuri <punsalang.zachary@gmail.com>
2023-11-07 21:03:53 -0800

    mainNav config: removed position

--- src/config/main.ts
+++ src/config/main.ts
@@ -5,22 +5,18 @@ export const mainConfig: MainConfig = {
     {
       title: 'Who',
       href: '/',
-      position: -11.2
     },
     {
       title: 'What',
       href: '/what',
-      position: 43.6
     },
     {
       title: 'Works',
       href: '/works',
-      position: 102.9
     },
     {
       title: 'Blog',
       href: '/blog',
-      position: 168.6
     }
     // {
     //   title: 'Contact',
--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -1,67 +1,66 @@
-import { User } from "@prisma/client";
+import { User } from '@prisma/client';
 
-import { Icons } from "@/components/ui/icons";
+import { Icons } from '@/components/ui/icons';
 
 export type NavItem = {
-	title: string;
-	href: string;
-	position: number;
-	disabled?: boolean;
+  title: string;
+  href: string;
+  disabled?: boolean;
 };
 
 export type MainNavItem = NavItem;
 
 export type SidebarNavItem = {
-	title: string;
-	disabled?: boolean;
-	external?: boolean;
-	icon?: keyof typeof Icons;
+  title: string;
+  disabled?: boolean;
+  external?: boolean;
+  icon?: keyof typeof Icons;
 } & (
-	| {
-			href: string;
-			items?: never;
-	  }
-	| {
-			href?: string;
-			items: NavLink[];
-	  }
+  | {
+      href: string;
+      items?: never;
+    }
+  | {
+      href?: string;
+      items: NavLink[];
+    }
 );
 
 export type SiteConfig = {
-	name: string;
-	description: string;
-	url: string;
-	ogImage: string;
-	links: {
-		github: string;
-		linkedin: string;
-		twitter: string;
-		mail: string;
-	};
+  name: string;
+  description: string;
+  url: string;
+  ogImage: string;
+  links: {
+    github: string;
+    linkedin: string;
+    twitter: string;
+    mail: string;
+  };
 };
 
 export type DocsConfig = {
-	mainNav: MainNavItem[];
-	sidebarNav: SidebarNavItem[];
+  mainNav: MainNavItem[];
+  sidebarNav: SidebarNavItem[];
 };
 
 export type MainConfig = {
-	mainNav: MainNavItem[];
+  mainNav: MainNavItem[];
 };
 
 export type ContactItem = {
-	icon?: keyof typeof Icons;
-	name: string;
-	href: string;
-	description: string;
-	info: string;
+  icon?: keyof typeof Icons;
+  name: string;
+  href: string;
+  description: string;
+  info: string;
 };
 
 export type ContactConfig = {
-	contactItems: ContactItem[];
+  contactItems: ContactItem[];
 };
 
 export type DashboardConfig = {
-	mainNav: MainNavItem[];
-	sidebarNav: SidebarNavItem[];
+  mainNav: MainNavItem[];
+  sidebarNav: SidebarNavItem[];
 };
