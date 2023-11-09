3f14be7edf6f4e016a23d51a62b9705b8376f623
zachuri <punsalang.zachary@gmail.com>
2023-11-09 00:27:04 -0800

    three-model: updated code to be more simple

--- src/components/three-model.tsx
+++ src/components/three-model.tsx
@@ -1,53 +1,57 @@
 'use client';
 
-import { useRef } from 'react';
-import { Canvas, useFrame, useLoader } from '@react-three/fiber';
-import { OrbitControls, useGLTF } from '@react-three/drei';
-// @ts-ignore
-import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
-import { Mesh, Scene } from 'three';
-import { DRACOLoader } from 'three/examples/jsm/loaders/DRACOLoader.js';
+import { Suspense, useState } from 'react';
+import { Canvas, useFrame } from '@react-three/fiber';
+import { Environment, OrbitControls, useGLTF } from '@react-three/drei';
+import { Icons } from './ui/icons';
 
 function ModelMesh({ ...props }) {
-  const fileUrl = '/zachuri-person.glb';
-  const meshRef = useRef<Mesh>(null!);
+  const [isModelLoaded, setModelLoaded] = useState(false);
 
-  // const gltf = useLoader(GLTFLoader, fileUrl);
+  if (!isModelLoaded) {
+    setModelLoaded(true);
+  }
 
-  const gltf = useLoader(GLTFLoader, fileUrl, loader => {
-    // Load DRACOLoader
-    const dracoLoader = new DRACOLoader();
-    loader.dracoLoader = dracoLoader;
-    dracoLoader.setDecoderPath('/draco/');
-    dracoLoader.setDecoderConfig({ type: 'js' });
-    loader.setDRACOLoader(dracoLoader);
-  });
+  const { scene } = useGLTF('/dog.glb');
 
-  useFrame(({ camera }) => {
-    meshRef.current.rotation.y += 0.002;
-
-    camera.lookAt(0, 0, 0); // Look at the center of the model
+  // Add an automatic rotation using useFrame
+  useFrame(() => {
+    if (scene && isModelLoaded) {
+      scene.rotation.y += 0.002; // Adjust the rotation speed as needed
+    }
   });
 
   return (
-    <mesh {...props} ref={meshRef} castShadow receiveShadow>
-      <directionalLight
-        castShadow
-        position={[2.5, 8, 5]}
-        shadow-mapSize={[1024, 1024]}
-      />
-      <meshStandardMaterial />
-      <primitive object={gltf.scene} />
-    </mesh>
+    <>
+      {isModelLoaded ? (
+        <primitive object={scene} {...props} />
+      ) : (
+        <Icons.loading />
+      )}
+    </>
   );
 }
 
+export const angleToRadians = (angleInDeg: number) =>
+  (Math.PI / 180) * angleInDeg;
+
 export function ThreeModel() {
   return (
-    <Canvas>
-      <OrbitControls />
-      <ambientLight/>
-      <ModelMesh />
-    </Canvas>
+    <Suspense fallback={<Icons.loading />}>
+      <Canvas camera={{ position: [5, 5, -5], fov: 25 }}>
+        <ambientLight intensity={0.5} />
+        <ModelMesh
+          // position={[-0.1, -0.2, 0]}
+          rotation={[0, Math.PI / 2, 0]}
+          scale={0.2}
+        />
+        <Environment preset="city" />
+        <OrbitControls
+          minPolarAngle={Math.PI / 2.5}
+          maxPolarAngle={Math.PI / 2.5}
+        />
+        {/* <axesHelper args={[5]} /> */}
+      </Canvas>
+    </Suspense>
   );
 }
