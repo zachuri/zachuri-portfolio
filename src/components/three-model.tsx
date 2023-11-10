98a2f1957d47301932223593e2ae1b0dc4272639
zachuri <punsalang.zachary@gmail.com>
2023-11-09 17:03:40 -0800

    three-model: no zoom/ change camera angle

--- src/components/three-model.tsx
+++ src/components/three-model.tsx
@@ -2,58 +2,30 @@
 
 import { Suspense, useEffect, useState } from 'react';
 import { Canvas, useFrame } from '@react-three/fiber';
-import { Environment, OrbitControls, useGLTF } from '@react-three/drei';
+import {
+  Environment,
+  OrbitControls,
+  Preload,
+  useGLTF,
+  useProgress
+} from '@react-three/drei';
 import { Icons } from './ui/icons';
+import { progress } from 'framer-motion';
 
 function ModelMesh({ ...props }) {
-  const [isModelLoaded, setModelLoaded] = useState(false);
-  const [initialSpinFrames, setInitialSpinFrames] = useState(100);
-  const [currentFrame, setCurrentFrame] = useState(0);
-  const [initialSpinSpeed, setInitialSpinSpeed] = useState(0.02);
-
-  useEffect(() => {
-    const handleResize = () => {
-      const xsScreenThreshold = 600; // Adjust this threshold as needed for your design
-      const newInitialSpinSpeed =
-        window.innerWidth <= xsScreenThreshold ? 0.6 : 0.25;
-      setInitialSpinSpeed(newInitialSpinSpeed);
-    };
-
-    handleResize(); // Call it initially to set the initial spin speed
-    window.addEventListener('resize', handleResize);
-
-    return () => {
-      window.removeEventListener('resize', handleResize);
-    };
-  }, []);
-
-  if (!isModelLoaded) {
-    setModelLoaded(true);
-  }
-
   const { scene } = useGLTF('/zachuri-person.glb');
 
   useFrame(() => {
-    if (scene && isModelLoaded) {
-      if (currentFrame < initialSpinFrames) {
-        // Initial spin logic
-        scene.rotation.y += initialSpinSpeed;
-        setCurrentFrame(prevFrame => prevFrame + 1);
-      } else {
-        // Normal rotation logic
-        const normalRotationSpeed = 0.002;
-        scene.rotation.y += normalRotationSpeed;
-      }
+    if (scene) {
+      scene.rotation.y += 0.002;
     }
   });
 
   return (
     <>
-      {isModelLoaded ? (
+      <mesh>
         <primitive object={scene} {...props} />
-      ) : (
-        <Icons.loading />
-      )}
+      </mesh>
     </>
   );
 }
@@ -62,16 +34,27 @@ export const angleToRadians = (angleInDeg: number) =>
   (Math.PI / 180) * angleInDeg;
 
 export function ThreeModel() {
+  const { progress } = useProgress();
+
   return (
-    <Suspense fallback={<Icons.loading />}>
-      <Canvas camera={{ position: [5, 5, -5], fov: 8 }}>
+    <Suspense
+      fallback={
+        <p className="flex flex-col">
+          <Icons.loading />
+          {progress.toFixed(2)}%
+        </p>
+      }
+    >
+      <Canvas camera={{ fov: 12 }} shadows>
         <ModelMesh rotation={[0, Math.PI / 8, 0]} scale={0.2} />
         <Environment preset="sunset" />
         <OrbitControls
+          enableZoom={false}
           minPolarAngle={Math.PI / 2.5}
           maxPolarAngle={Math.PI / 2.5}
         />
-        {/* <axesHelper args={[5]} /> */}
+        <axesHelper args={[5]} />
+        <Preload all />
       </Canvas>
     </Suspense>
   );
