5471cb8d558895a11ba9d8ee786ffb6a043b8ea2
zachuri <punsalang.zachary@gmail.com>
2023-11-09 20:32:53 -0800

    navbar: chagned to rounded lg'

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -9,7 +9,7 @@ const Navbar = () => {
     <>
       <header className="sticky top-0 z-40">
         <div className="p-5 flex justify-center items-center">
-          <div className="flex flex-row items-center justify-center p-1 gap-x-1 rounded-xl bg-primary">
+          <div className="flex flex-row items-center justify-center p-1 gap-x-1 rounded-lg bg-primary">
             <Button size="sm">
               <Link href="/">
                 <Icons.logo />
