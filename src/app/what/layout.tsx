3c89ecea8c5eb5292638645b66bd9ae174048a1b
ZACHURI <punsalang.zachary@gmail.com>
2023-11-15 14:19:47 -0800

    what: adding side bar to be sticky (working)

--- src/app/what/layout.tsx
+++ src/app/what/layout.tsx
@@ -1,46 +1,43 @@
-import PageAnimation from '@/components/animation/page-animation';
-import MainContainer from '@/components/layout/main-container';
-import { SiteFooter } from '@/components/site-footer';
+import PageAnimation from "@/components/animation/page-animation";
+import MainContainer from "@/components/layout/main-container";
+import { SiteFooter } from "@/components/site-footer";
 
 interface MainLayoutProps {
-  children: React.ReactNode;
+	children: React.ReactNode;
 }
 
 const features = [
-  {
-    title: 'Technical Skills',
-    id: 'Technical Skills'
-  },
-  {
-    title: 'Wolk Flow',
-    id: 'work-flow'
-  },
-  {
-    title: 'Personal Interests',
-    id: 'personal-interests'
-  }
+	{
+		title: "Technical Skills",
+		id: "Technical Skills",
+	},
+	{
+		title: "Wolk Flow",
+		id: "work-flow",
+	},
+	{
+		title: "Personal Interests",
+		id: "personal-interests",
+	},
 ];
 
 export default async function Layout({ children }: MainLayoutProps) {
-  return (
-    <div className="flex flex-col space-y-6">
-      <div className="container grid flex-1 items-start lg:grid-cols-[20pc_80pc]">
-        {/* Left side takes 20% of the width */}
-        <aside className="flex aspect-w-5 md:hidden lg:flex sticky top-16 w-full flex-col">
-          <ul>
-            {features.map(feature => {
-              return <li key={feature.id}>{feature.title}</li>;
-            })}
-          </ul>
-        </aside>
-        {/* Right side takes 80% of the width */}
-        <main className="relative flex w-full flex-col">
-          <div className="flex flex-col justify-center items-center w-full max-md:my-10">
-            <PageAnimation>{children}</PageAnimation>
-          </div>
-        </main>
-      </div>
-      <SiteFooter className="border-t" />
-    </div>
-  );
+	return (
+		<div className='flex w-full items-start'>
+			<div className='w-1/5 mx-auto bg-gray-500 flex h-screen sticky top-[64px]'>
+				<div className='relative w-full bg-white p-4'>
+					<ul>
+						{features.map(feature => {
+							return <li key={feature.id}>{feature.title}</li>;
+						})}
+					</ul>
+				</div>
+			</div>
+			<div className='w-4/5 bg-blue-500'>
+				<div className='w-3/4 bg-white p-4'>
+					<PageAnimation>{children}</PageAnimation>
+				</div>
+			</div>
+		</div>
+	);
 }
--- src/app/what/page.tsx
+++ src/app/what/page.tsx
@@ -1,43 +1,44 @@
-'use client';
+"use client";
 
-import React from 'react';
-import Skills from '@/components/skills';
-import { ThreeModel } from '@/components/three/three-model';
+import React from "react";
+import Skills from "@/components/skills";
+import { ThreeModel } from "@/components/three/three-model";
 import {
-  Availability,
-  Colors,
-  Music,
-  SchedulingLinks,
-  Team,
-  Todo
-} from '@/components/features/card';
-import FeatureTitle from '@/components/features/title';
-import Feature from '@/components/features/feature';
+	Availability,
+	Colors,
+	Music,
+	SchedulingLinks,
+	Team,
+	Todo,
+} from "@/components/features/card";
+import FeatureTitle from "@/components/features/title";
+import Feature from "@/components/features/feature";
+import { SiteFooter } from "@/components/site-footer";
 
 function H1({ children }: { children: React.ReactNode }) {
-  return <h1 className="text-2xl md:text-3xl font-bold">{children}</h1>;
+	return <h1 className='text-2xl md:text-3xl font-bold'>{children}</h1>;
 }
 
 function H2({ children }: { children: React.ReactNode }) {
-  return <h2 className="text-xl md:text-2xl font-semibold">{children}</h2>;
+	return <h2 className='text-xl md:text-2xl font-semibold'>{children}</h2>;
 }
 
 const Page: React.FC = () => {
-  return (
-    <div className="max-sm:-mt-10 flex flex-col space-y-10">
-      <section className="flex flex-col max-md:items-center max-md:justify-center">
-        <div className="w-[80%] sm:-mb-52 -mb-32 z-10">
-          <center className="h-[400px] sm:h-[600px] flex items-center justify-center">
-            <ThreeModel />
-          </center>
-        </div>
-      </section>
-      <Skills />
+	return (
+		<div className='max-sm:-mt-10 flex flex-col space-y-10'>
+			<section className='flex flex-col items-center justify-center'>
+				<div className='w-[80%] sm:-mb-52 -mb-32 z-10'>
+					<center className='h-[400px] sm:h-[600px] flex items-center justify-center'>
+						<ThreeModel />
+					</center>
+				</div>
+			</section>
+			<Skills />
 
-      {/* <Feature features={features} /> */}
-      <Feature />
-    </div>
-  );
+			{/* <Feature features={features} /> */}
+			<Feature />
+		</div>
+	);
 };
 
 export default Page;
