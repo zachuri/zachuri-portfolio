0a4d2ad48da925168ebef78a841a74c98960bf15
zachuri <punsalang.zachary@gmail.com>
2023-11-16 15:44:31 -0800

    what: fix layout for interests/ changed roundness to sm

--- src/app/what/page.tsx
+++ src/app/what/page.tsx
@@ -89,12 +89,12 @@ const Page: React.FC = () => {
       </Section>
 
       <Section id={'interests'} className="pb-32">
-        <h1 className="text-2xl md:text-3xl font-bold">Interests</h1>
+        <h1 className="text-2xl md:text-3xl font-bold pb-5">Interests</h1>
         <div className="grid grid-cols-2 gap-4">
           <div className="grid gap-4">
             <div>
               <Image
-                className="h-auto max-w-full rounded-lg"
+                className="h-auto max-w-full rounded-sm"
                 src="/assets/keyboards/jp-02.jpeg"
                 alt="jp-02"
                 width={500}
@@ -103,7 +103,7 @@ const Page: React.FC = () => {
             </div>
             <div>
               <Image
-                className="h-auto max-w-full rounded-lg"
+                className="h-auto max-w-full rounded-sm"
                 src="/assets/keyboards/tofu60.jpeg"
                 alt="tofu-60"
                 width={500}
@@ -112,7 +112,7 @@ const Page: React.FC = () => {
             </div>
             <div>
               <Image
-                className="h-auto max-w-full rounded-lg"
+                className="h-auto max-w-full rounded-sm"
                 src="https://novelkeys.com/cdn/shop/files/TypeK_16x9-4_1024x1024@2x.jpg?v=1691498876"
                 alt="tofu-60"
                 width={500}
@@ -123,7 +123,7 @@ const Page: React.FC = () => {
           <div className="grid gap-4">
             <div>
               <Image
-                className="h-auto max-w-full rounded-lg"
+                className="h-auto max-w-full rounded-sm"
                 src="https://ssb.wiki.gallery/images/thumb/1/16/Marth_Idle_Pose_Melee.gif/200px-Marth_Idle_Pose_Melee.gif"
                 alt="jp-02"
                 width={500}
@@ -132,7 +132,7 @@ const Page: React.FC = () => {
             </div>
             <div>
               <Image
-                className="h-auto max-w-full rounded-lg"
+                className="h-auto max-w-full rounded-sm"
                 src="/assets/interests/samurai-champloo.jpg"
                 alt="samurai champloo"
                 width={500}
@@ -141,7 +141,7 @@ const Page: React.FC = () => {
             </div>
             <div>
               <Image
-                className="h-auto max-w-full rounded-lg"
+                className="h-auto max-w-full rounded-sm"
                 src="/assets/interests/cowboy-bebop.jpg"
                 alt="cowboy bebop"
                 width={500}
