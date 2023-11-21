f21319d24cc3ef845940b03ca49996586f5a9559
zachuri <punsalang.zachary@gmail.com>
2023-11-21 01:45:20 -0800

    what/ layout: moved pageanimation to children only

--- src/app/what/layout.tsx
+++ src/app/what/layout.tsx
@@ -10,26 +10,24 @@ interface MainLayoutProps {
 export default async function Layout({ children }: MainLayoutProps) {
   return (
     <div>
-      <PageAnimation>
-        <div className="flex w-full min-h-screen md:items-start grow">
-          <div className="w-1/5 sticky top-16 hidden md:block">
-            <div className="relative w-full pl-[8vw]">
-              <ul>
-                {sections.map(section => (
-                  <SectionTitle id={section.id} key={section.id}>
-                    {section.title}
-                  </SectionTitle>
-                ))}
-              </ul>
-            </div>
-          </div>
-          <div className="md:w-4/5 relative flex w-full flex-col">
-            <div className="md:w-3/4 flex flex-col justify-center items-center max-md:my-10 max-md:container">
-              {children}
-            </div>
+      <div className="flex w-full min-h-screen md:items-start grow">
+        <div className="w-1/5 sticky top-16 hidden md:block">
+          <div className="relative w-full pl-[8vw]">
+            <ul>
+              {sections.map(section => (
+                <SectionTitle id={section.id} key={section.id}>
+                  {section.title}
+                </SectionTitle>
+              ))}
+            </ul>
           </div>
         </div>
-      </PageAnimation>
+        <div className="md:w-4/5 relative flex w-full flex-col">
+          <div className="md:w-3/4 flex flex-col justify-center items-center max-md:my-10 max-md:container">
+            <PageAnimation>{children}</PageAnimation>
+          </div>
+        </div>
+      </div>
       <SiteFooter />
     </div>
   );
