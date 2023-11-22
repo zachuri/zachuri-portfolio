3d69ba6bf569680fdf977ffcdf8520c8d57135f9
zachuri <punsalang.zachary@gmail.com>
2023-11-22 02:34:35 -0800

    works: updated mobile to smaller

--- src/app/works/page.tsx
+++ src/app/works/page.tsx
@@ -24,14 +24,14 @@ export default async function Page() {
   const images = await getImages(worksConfig.works);
 
   return (
-    <div className={'mt-[60px] grid py-10 md:mt-[82.5px] w-full'}>
-      <div className="grid grid-cols-2 gap-4 md:gap-5 lg:gap-10 px-10 md:grid-cols-3 xl:grid-cols-4">
-        <h1 className="col-span-2 text-4xl uppercase text-primary max-md:text-3xl md:col-span-3 xl:col-span-4 font-medium">
+    <div className={'mt-[30px] grid py-10 md:mt-[82.5px] w-full'}>
+      <div className="grid grid-cols-1 sm:grid-cols-2 gap-4 md:gap-5 lg:gap-10 px-10 md:grid-cols-3 xl:grid-cols-4">
+        <h1 className="text-4xl uppercase text-primary max-md:text-3xl sm:col-span-2 md:col-span-3 xl:col-span-4 font-medium">
           Projects
         </h1>
         {images.map(({ base64, img }, index) => (
           <div key={img.id} className="space-y-2">
-            <div className="relative aspect-square h-auto border-[1px] rounded-md ">
+            <div className="relative aspect-square h-auto border-[1px] rounded-sm ">
               <Image
                 src={img.src}
                 alt={img.src}
@@ -42,13 +42,11 @@ export default async function Page() {
                 blurDataURL={base64}
                 layout="fill"
                 objectFit="cover"
-                className="rounded-md"
+                className="rounded-sm"
               />
             </div>
             <h2 className="text-md lg:text-2xl font-medium">{img.title}</h2>
-            <p className="hidden md:block max-xl:text-xs lg:text-md">
-              {img.desc}
-            </p>
+            <p className="text-xs md:text-sm">{img.desc}</p>
           </div>
         ))}
       </div>
