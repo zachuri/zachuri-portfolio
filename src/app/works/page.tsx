777ea3cf098b36525cca6b8bac2225e78c339f08
zachuri <punsalang.zachary@gmail.com>
2023-11-22 02:44:31 -0800

    works: updated extra small font to work with md/ added mx for mobile only

--- src/app/works/page.tsx
+++ src/app/works/page.tsx
@@ -25,12 +25,12 @@ export default async function Page() {
 
   return (
     <div className={'mt-[30px] grid py-10 md:mt-[82.5px] w-full'}>
-      <div className="grid grid-cols-1 sm:grid-cols-2 gap-4 md:gap-5 lg:gap-10 px-10 md:grid-cols-3 xl:grid-cols-4">
+      <div className="max-sm:mx-10 grid grid-cols-1 sm:grid-cols-2 gap-4 md:gap-5 lg:gap-10 px-10 md:grid-cols-3 xl:grid-cols-4">
         <h1 className="text-4xl uppercase text-primary max-md:text-3xl sm:col-span-2 md:col-span-3 xl:col-span-4 font-medium">
           Projects
         </h1>
         {images.map(({ base64, img }, index) => (
-          <div key={img.id} className="space-y-2">
+          <div key={img.id} className="space-y-2 mb-10">
             <div className="relative aspect-square h-auto border-[1px] rounded-sm ">
               <Image
                 src={img.src}
@@ -46,7 +46,7 @@ export default async function Page() {
               />
             </div>
             <h2 className="text-md lg:text-2xl font-medium">{img.title}</h2>
-            <p className="text-xs md:text-sm">{img.desc}</p>
+            <p className="text-xs lg:text-sm">{img.desc}</p>
           </div>
         ))}
       </div>
