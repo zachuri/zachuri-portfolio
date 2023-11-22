69f9dccaf683f21104dd6eb3c782dc5d8b1fe2f0
zachuri <punsalang.zachary@gmail.com>
2023-11-22 01:32:19 -0800

    layout: updated container to be for sm too

--- src/app/works/layout.tsx
+++ src/app/works/layout.tsx
@@ -9,7 +9,7 @@ interface Props {
 const MainLayout: React.FC<Props> = ({ children }) => {
   return (
     <div className="flex min-h-screen flex-col space-y-6">
-      <div className="grow sm:container sm:flex-1">
+      <div className="grow md:container sm:flex-1">
         <PageAnimation>{children}</PageAnimation>
       </div>
       <SiteFooter />
