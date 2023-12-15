fdcf82cded64767345083e5be48b9f53bad74ba5
zachuri <punsalang.zachary@gmail.com>
2023-12-14 18:19:49 -0800

    mast-head: added back button to works

--- src/components/works/mast-head.tsx
+++ src/components/works/mast-head.tsx
@@ -1,21 +1,28 @@
-import React from 'react';
+import React from "react";
+import { Button } from "../ui/button";
+import Link from "next/link";
+import { Icons } from "../ui/icons";
 
 export type MastHeadProps = {
-  title: string;
-  description: string;
+	title: string;
+	description: string;
 };
 
 const MastHead: React.FC<MastHeadProps> = ({ title, description }) => {
-  return (
-    <section className="space-y-5">
-      <h1 className="text-3xl font-medium">{title}</h1>
-      <p
-        dangerouslySetInnerHTML={{
-          __html: description.replace(/\n/g, '<br/>')
-        }}
-      />
-    </section>
-  );
+	return (
+		<section className='space-y-5'>
+			<Button variant={"secondary"} className='text-xs'>
+				<Icons.chevronLeft size={15} />
+				<Link href={"/works"}>See all works</Link>
+			</Button>
+			<h1 className='text-3xl font-medium'>{title}</h1>
+			<p
+				dangerouslySetInnerHTML={{
+					__html: description.replace(/\n/g, "<br/>"),
+				}}
+			/>
+		</section>
+	);
 };
 
 export default MastHead;
