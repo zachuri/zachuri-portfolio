2ce885cc77abbb2a64a2e7b6d9de0040ac8c1bdb
zachuri <punsalang.zachary@gmail.com>
2023-12-14 22:23:50 -0800

    blog: general setup working

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,9 @@
 /** @type {import('next').NextConfig} */
-import withPlaiceholder from '@plaiceholder/next';
+import withPlaiceholder from "@plaiceholder/next";
+import { withContentlayer } from "next-contentlayer";
 
 const nextConfig = {};
 
 // module.exports = nextConfig;
 
-export default withPlaiceholder(nextConfig);
+export default withPlaiceholder(withContentlayer(nextConfig));
