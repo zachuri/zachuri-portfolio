02fc584500117e913db37cac27eddbd8beadb71c
zachuri <punsalang.zachary@gmail.com>
2024-02-11 01:56:44 -0800

    zachuri: what-section changed size for nav link hover title size

--- src/components/what/section-title.tsx
+++ src/components/what/section-title.tsx
@@ -1,31 +1,30 @@
-'use client';
+"use client";
 
-import { cn } from '@/lib/utils';
-import React from 'react';
-import { useFeatureStore } from '../store/feature';
+import { cn } from "@/lib/utils";
+import React from "react";
+import { useFeatureStore } from "../store/feature";
 
 export type FeatureCardProps = {
-  children: React.ReactNode;
+	children: React.ReactNode;
 } & CardProps;
 
 export type CardProps = {
-  id: string;
+	id: string;
 };
 
 const SectionTitle: React.FC<FeatureCardProps> = ({ id, children }) => {
-  const inViewFeature = useFeatureStore(state => state.inViewFeature);
+	const inViewFeature = useFeatureStore(state => state.inViewFeature);
 
-  return (
-    <p
-      key={id}
-      className={cn(
-        'font-heading text-[1.5vw] lg:text-[1.2vw] uppercase transition-colors text-gray-300',
-        inViewFeature === id ? 'text-primary' : 'text-primary/20'
-      )}
-    >
-      {children}
-    </p>
-  );
+	return (
+		<p
+			key={id}
+			className={cn(
+				"font-heading text-[1.5vw] lg:text-[1.2vw] 2xl:text-[1vw] uppercase transition-colors text-gray-300",
+				inViewFeature === id ? "text-primary" : "text-primary/20"
+			)}>
+			{children}
+		</p>
+	);
 };
 
 export default SectionTitle;
