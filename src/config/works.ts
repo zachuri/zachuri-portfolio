cf46d9d159f3bda8d3848d16ab733b2a77dec886
zachuri <punsalang.zachary@gmail.com>
2024-11-15 14:04:06 -0800

    chore: fix typo

--- src/config/works.ts
+++ src/config/works.ts
@@ -23,7 +23,7 @@ export const worksConfig: WorkConfig = {
 		{
 			id: "lightstrike",
 			title: "STAR/CHILD",
-			desc: "Tehcnical Project Manager\nQaulity Assurance Engineer\nSoftware Engineer",
+			desc: "Technical Project Manager\nQaulity Assurance Engineer\nSoftware Engineer",
 			src: "/assets/experience/starchild-01.jpeg",
 			website: "https://www.starchild.app/",
 		},
