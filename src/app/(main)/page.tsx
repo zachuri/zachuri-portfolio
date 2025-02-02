d41fe0811083d8f90fd604f2c7c051a425b29c48
zachuri <punsalang.zachary@gmail.com>
2025-02-02 09:53:52 -0800

    chore: keep content relative within main

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -16,16 +16,18 @@ const words = [
 
 export default function Home() {
 	return (
-		<section className='container space-y-6 lg:space-y-14 min-h-screen flex flex-col items-center justify-center'>
-			<div className='mx-auto flex max-w-[58rem] flex-col items-center space-y-2 text-center gap-5 '>
-				<h2 className='font-heading font-semibold leading-[1.1] text-6xl'>
-					Zachary Punsalang
-				</h2>
-				<div className='flex justify-center items-center w-full leading-normal text-muted-foreground sm:leading-7 text-lg md:text-xl p-5'>
-					<FlipWords words={words} />
+		<section className='container gap-6 lg:gap-14 min-h-screen flex flex-col items-center justify-center'>
+			<div className='relative flex flex-col items-center justify-center gap-5'>
+				<div className='mx-auto flex max-w-[58rem] flex-col items-center space-y-2 text-center gap-5'>
+					<h2 className='font-heading font-semibold leading-[1.1] text-6xl'>
+						Zachary Punsalang
+					</h2>
 				</div>
+				<div className='text-lg mx-auto font-normal text-neutral-600 dark:text-neutral-400'>
+					<FlipWords words={words} /> <br />
+				</div>
+				<MainContact items={contactConfig.contactItems} />
 			</div>
-			<MainContact items={contactConfig.contactItems} />
 		</section>
 	);
 }
