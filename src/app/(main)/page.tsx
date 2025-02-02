97124977d276b8b45a2797d4887c5346c85b32e5
zachuri <punsalang.zachary@gmail.com>
2025-02-02 08:11:57 -0800

    chore: update layout for who

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -2,6 +2,7 @@
 
 import MainContact from "@/components/main-contact";
 import { FlipWords } from "@/components/ui/flip-words";
+import { Separator } from "@/components/ui/separator";
 import { contactConfig } from "@/config/contact";
 
 const words = [
@@ -14,15 +15,14 @@ const words = [
 export default function Home() {
 	return (
 		<section className='container space-y-6 lg:space-y-14 min-h-screen flex flex-col items-center justify-center'>
-			<div className='mx-auto flex max-w-[58rem] flex-col items-center space-y-2 text-center'>
+			<div className='mx-auto flex max-w-[58rem] flex-col items-center space-y-2 text-center gap-5'>
 				<h2 className='font-heading font-semibold leading-[1.1] text-6xl'>
 					Zachary Punsalang
 				</h2>
 				<div className='max-w-[85%] leading-normal text-muted-foreground sm:leading-7 text-lg md:text-xl p-5'>
-					<FlipWords words={words} /> <br />
+					<FlipWords words={words} />
 				</div>
 			</div>
-
 			<MainContact items={contactConfig.contactItems} />
 		</section>
 	);
