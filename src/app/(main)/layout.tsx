f6ab9ed3c197198463b53e3ccbf42462ded8294d
zachuri <punsalang.zachary@gmail.com>
2025-02-02 07:22:44 -0800

    chore: updated metadata to something simple

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -8,7 +8,7 @@ interface MainLayoutProps {
 export const metadata: Metadata = {
 	title: "Zachary Punsalang - Homepage",
 	description:
-		"I'm a Computer Science graduate from the University of California, Irvine, driven by a passion for building meaningful projects that positively impact people's lives. My curiosity about how things are created from the ground up inspires me to explore and understand the intricate details of technology. I’m also fascinated by how coding allows us to express ideas and bring them to life. As a developer, I’m always eager to learn new technologies, embrace innovative ideas, and grow through collaboration and challenges.",
+		"Zachary is a Software Engineer / Full-stack developer based in California with a passion for building digital services/stuff he wants.",
 };
 
 export default async function MainLayout({ children }: MainLayoutProps) {
