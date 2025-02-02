6457c8cc330dad15a76adbb99c98a49d6ef633cc
zachuri <punsalang.zachary@gmail.com>
2025-02-02 09:22:50 -0800

    chore: updated shadow to md

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -9,7 +9,7 @@ const Navbar = () => {
 		<>
 			<header className='sticky top-0 z-40'>
 				<div className='pt-5 flex justify-center items-center'>
-					<div className='flex flex-row items-center justify-center p-1 gap-x-1 rounded-xl bg-primary shadow-lg dark:shadow-gray-600'>
+					<div className='flex flex-row items-center justify-center p-1 gap-x-1 rounded-xl bg-primary shadow-md dark:shadow-gray-600'>
 						<Button size='sm'>
 							<Link href='/'>
 								<Icons.logo />
