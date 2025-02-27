e97cfb137127147c51bc5e94561957533a0c17a8
zac <punsalang.zachary@gmail.com>
2025-02-27 14:28:43 -0800

    update(skills): current 2025

--- src/config/skills.ts
+++ src/config/skills.ts
@@ -42,24 +42,25 @@ export const skillsConfig: SkillConfig = {
 		{ name: "Three.js", type: 1 }, // Frontend
 
 		// Backend
-		{ name: "RESTful API", type: 5 }, // Backend
-		{ name: "AWS Web Services", type: 5 }, // Backend
-		{ name: "tRPC", type: 5 }, // Backend
-		{ name: "Node.js", type: 5 }, // Backend
 		{ name: "Express.js", type: 5 }, // Backend
+		{ name: "Hono.js ", type: 5 }, // Backend
+		{ name: "tRPC", type: 5 }, // Backend
 		{ name: "Deno.js", type: 5 }, // Backend
 		{ name: "Nest.js", type: 5 }, // Backend
 		{ name: "Spring Boot", type: 5 }, // Backend
+		{ name: "Node.js", type: 5 }, // Backend
+		{ name: "RESTful API", type: 5 }, // Backend
+		{ name: "AWS Web Services", type: 5 }, // Backend
 		{ name: "GraphQL", type: 5 }, // Backend
 
 		// Databases
-		{ name: "MySQL", type: 4 }, // Database
 		{ name: "Postgres", type: 4 }, // Database
-		{ name: "MongoDB", type: 4 }, // Database
 		{ name: "Supabase", type: 4 }, // Database
-		{ name: "Firebase", type: 4 }, // Database
 		{ name: "Prisma", type: 4 }, // Database
 		{ name: "Drizzle", type: 4 }, // Database
+		{ name: "MySQL", type: 4 }, // Database
+		{ name: "MongoDB", type: 4 }, // Database
+		{ name: "Firebase", type: 4 }, // Database
 		{ name: "JDBC", type: 4 }, // Database
 		{ name: "Vercel", type: 4 }, // Database
 		{ name: "AWS", type: 4 }, // Database
@@ -75,6 +76,7 @@ export const skillsConfig: SkillConfig = {
 		{ name: "Jest", type: 2 }, // Tool
 
 		// Other
+		{ name: "Monorepo", type: 3 }, // Other
 		{ name: "Agile", type: 3 }, // Other
 		{ name: "Machine Learning", type: 3 }, // Other
 		{ name: "Data Structures", type: 3 }, // Other
